9bd5ece0e1071ca4a6c7f35f532526626b79618e
Buj Itself <primary@buj-dev.site>
2022-10-30 10:37:27 +0700

    Development commit 2

--- client/inject/java/net/minecraft/client/rmlcompat/Blocks.java
+++ client/inject/java/net/minecraft/client/rmlcompat/Blocks.java
@@ -1,8 +1,13 @@
 package net.minecraft.client.rmlcompat;
 
 import net.buj.rml.annotations.Nullable;
+import net.buj.rml.registry.Items;
 
 public class Blocks extends net.buj.rml.registry.Blocks {
+    public Blocks(Items source) {
+        super(source);
+    }
+
     @Override
     protected @Nullable Block createBlock(int id) {
         return new net.minecraft.src.Block(id);
