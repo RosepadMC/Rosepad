a280121651bacb09e9c41a477351b8e228086f50
Buj <buj@buj-dev.ru>
2023-08-09 03:01:45 +0700

    Removed legacy buttons

--- client/inject/java/net/minecraft/src/GuiOptionsButton.java
+++
@@ -1,6 +0,0 @@
-package net.minecraft.src;
-
-public interface GuiOptionsButton {
-    String getText();
-    void run(GuiButton button, GuiScreen parent);
-}
--- client/inject/java/net/minecraft/src/GuiOptionsContext.java
+++
@@ -1,9 +0,0 @@
-package net.minecraft.src;
-
-import java.util.*;
-
-public class GuiOptionsContext {
-	public String title;
-	public List<GuiOptionsButton> topButtons;
-	public List<GuiOptionsButton> bottomButtons;
-}
